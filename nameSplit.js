40632d002639ed7daf95ee2bfb62db6b0be118af
vinnynarula1 <vinny.narula7@gmail.com>
2022-03-29 16:17:04 -0700

    changes

---
+++ nameSplit.js
@@ -0,0 +1,13 @@
+let myArray = [];
+myArray = ['Fake Name', 'blue'];
+
+function cutName(name) {
+    const arr = name.split(" ");
+    return arr;
+};
+let myInfo = {};
+myInfo = {
+    fullName: myArray[0],
+    favoriteColor: myArray[1],
+    github: null
+};
